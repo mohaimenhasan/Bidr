353afc58faafb7cd202acee92e68b1b9a77b43de
Mohaimen Khan <mohaimenhasan@gmail.com>
2023-04-11 22:15:37 -0400

    Created a nav bar with user profle

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import logo from '../logobidr.svg';
 import { Auth } from 'aws-amplify';
@@ -7,6 +7,8 @@ import { Auth } from 'aws-amplify';
 function Navbar() {
   const navigate = useNavigate();
   const [authenticated, setAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
 
   useEffect(() => {
     checkAuthStatus();
@@ -15,7 +17,7 @@ function Navbar() {
   const checkAuthStatus = async () => {
     try {
       const user = await Auth.currentAuthenticatedUser();
-      console.log(user);
+      setUser(user);
       setAuthenticated(true);
     } catch (err) {
       setAuthenticated(false);
@@ -40,17 +42,54 @@ function Navbar() {
     }
   };
 
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleProfileClick = () => {
+    // navigate to user profile page
+    handleMenuClose();
+  };
+
+  const handleSettingsClick = () => {
+    // navigate to user settings page
+    handleMenuClose();
+  };
+
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
+  const handleDashboardClick = () => {
+    navigate('/dashboard');
+  }
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <img src={logo} alt="Bidr logo" style={{ height: '2rem', maxWidth: '100%', marginRight: '0.5rem' }} />
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+        <img src={logo} alt="Bidr logo" style={{ height: '2rem', maxWidth: '100%', marginRight: '0.5rem', cursor: 'pointer' }} onClick={handleLogoClick} />
+        <Typography variant="h6" sx={{ flexGrow: 1, cursor: 'pointer' }} onClick={handleLogoClick}>
           Bidr
         </Typography>
         {authenticated ? (
-          <Button color="inherit" onClick={handleLogout}>
-            Logout
-          </Button>
+          <>
+            <IconButton color="inherit" onClick={handleMenuOpen}>
+              <Typography variant="subtitle1" sx={{ textTransform: 'none', marginRight: '0.5rem' }}>
+                {user !== null ? user.attributes.given_name : null}
+              </Typography>
+              <i className="fas fa-caret-down"></i>
+            </IconButton>
+            <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+              <MenuItem onClick={handleDashboardClick}>Dashboard</MenuItem>
+              <MenuItem onClick={handleProfileClick}>Profile</MenuItem>
+              <MenuItem onClick={handleSettingsClick}>Settings</MenuItem>
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+          </>
         ) : (
           <>
             <Button color="inherit" onClick={handleLogin}>
